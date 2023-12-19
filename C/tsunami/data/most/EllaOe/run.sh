9491e262d0eb34d23b0e723dfd63bd87d3df06d4
sylfest <40855093+sylfest@users.noreply.github.com>
2023-12-19 11:24:47 +0100

    Delete C directory

--- C/tsunami/data/most/EllaOe/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for r in 2m 1m 
-do
-gmt grdsample ella_oe_base_area_utm27.nc -Rell$r\.nc -Gtmp.nc
-gmt grdclip -Sb0/NAN tmp.nc -Gtmp2.nc
-gmt grdmath tmp2.nc -1 MUL ell$r\.nc AND = ella_$r\_utm27.nc
-done
--- C/tsunami/sim/most/maxeta.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-
-tide=1.6
-#tide=0.7
-nc_maxeta.py -f sim_runup_ha.nc -s maxeta_tide$tide\.nc
-nc_gridadj3.py -f maxeta_tide$tide\.nc -s tmp222.nc -x 1000 -c Karr -r
-gmt grdmath tmp222.nc $tide ADD = tmp333.nc
-gmt grdclip -Sa999999/Nan tmp333.nc -Gmaxeta_tide$tide\_utm27_meter.nc
-gdal_translate maxeta_tide$tide\_utm27_meter.nc maxeta_tide$tide\_utm27_meter.asc
